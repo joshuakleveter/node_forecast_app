e53c64f50d9502a5f8f8ac4290f46f85d8e37b79
Joshua Kleveter <joshua.kleveter@gmail.com>
2016-01-08 17:23:58 -0600

    Wrote _getFile() function

--- src/js/renderer.js
+++ src/js/renderer.js
@@ -2,6 +2,7 @@
 //Imports //
 ////////////
 
+var fs = require("fs");
 
 
 
@@ -9,6 +10,15 @@
 //Renderer Module //
 ////////////////////
 
+function _getFile(filepath) {
+    var filePromise = new Promise(function (resolve, reject) {
+        fs.readFile(filepath, "utf8", function (error, data) {
+            if (error) reject(error);
+            resolve(data);
+        });
+    });
+    return filePromise;
+}
 
 
 
