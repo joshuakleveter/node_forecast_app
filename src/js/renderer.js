360fb94c2a35cc0084a397dd2abb0802937dd031
Joshua Kleveter <joshua.kleveter@gmail.com>
2016-01-08 17:38:59 -0600

    Added doc block

--- src/js/renderer.js
+++ src/js/renderer.js
@@ -10,6 +10,11 @@ var fs = require("fs");
 //Renderer Module //
 ////////////////////
 
+/**
+ * Get the contents of a file
+ * @param  {String} filepath - Path to file.
+ * @return {Promise}         - File promise
+ */
 function _getFile(filepath) {
     var filePromise = new Promise(function (resolve, reject) {
         fs.readFile(filepath, "utf8", function (error, data) {
