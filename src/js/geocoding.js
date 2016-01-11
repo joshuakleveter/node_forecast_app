255949f1a00cca93f2de827e324c4986e3de265d
Joshua Kleveter <joshua.kleveter@gmail.com>
2016-01-11 08:16:03 -0600

    Wrote geocode() generator

--- src/js/geocoding.js
+++ src/js/geocoding.js
@@ -9,6 +9,16 @@
 //Geocoding Module //
 /////////////////////
 
+/**
+ * Generate lat/long for a given ZIP code
+ * @param  {String} zip - ZIP code
+ * @return {Object}     - Lat/Long of the given ZIP code
+ */
+function* geocode(zip) {
+    var geocodeJSON = yield Promise.resolve(_geocodeRequest(zip));
+    var geocodeResponse = JSON.parse(geocodeJSON);
+    return geocodeResponse.results[0].geometry.location;
+}
 
 
 
