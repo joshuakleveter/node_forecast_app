934c128d66a146e27a8db3b60c6e020f4020da86
Joshua Kleveter <joshua.kleveter@gmail.com>
2016-01-11 08:26:24 -0600

    Wrote _geocodeRequest() function

--- src/js/geocoding.js
+++ src/js/geocoding.js
@@ -2,6 +2,7 @@
 //Imports //
 ////////////
 
+var https = require("https");
 
 
 
@@ -10,13 +11,40 @@
 /////////////////////
 
 /**
+ * Make a request to the Google Maps Geocoding API
+ * @param  {String} zip - ZIP code to geocode
+ * @return {Promise}    - Promise for API response
+ */
+function _geocodeRequest(zip) {
+    var geocodePromise = new Promise(function (resolve, reject) {
+        var key = "AIzaSyAqOAWv-714TAq0SdCwn9BogqzRuhPjA2A";
+        var url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + zip + "&key=" + key;
+
+        var apiRequest = https.get(url, function (response) {
+            var responseJSON = "";
+            response.on("data", function (data) {
+                responseJSON += data;
+            });
+            response.on("end", function () {
+                resolve(JSON.parse(responseJSON));
+            });
+        });
+        apiRequest.on("error", function (error) {
+            reject(error);
+        });
+    });
+    return geocodePromise;
+}
+
+
+
+/**
  * Generate lat/long for a given ZIP code
  * @param  {String} zip - ZIP code
  * @return {Object}     - Lat/Long of the given ZIP code
  */
 function* geocode(zip) {
-    var geocodeJSON = yield Promise.resolve(_geocodeRequest(zip));
-    var geocodeResponse = JSON.parse(geocodeJSON);
+    var geocodeResponse = yield Promise.resolve(_geocodeRequest(zip));
     return geocodeResponse.results[0].geometry.location;
 }
 
